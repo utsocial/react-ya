5545721dd4ceb72d9f373768e024a7f1b79ee6f0
Ivan Rogelio Chenoweth Chenoweth <ivan chenoweth@tiempodev.local>
2020-01-26 20:19:17 -0700

    US10 : peticiones ajax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
 import "./App.css";
-import Reloj from "./Reloj";
-//
-// http://www.tutorialesprogramacionya.com/reactya/detalleconcepto.php?punto=9&codigo=9&inicio=0
-// proyecto008
+
+// http://www.tutorialesprogramacionya.com/reactya/detalleconcepto.php?punto=10&codigo=10&inicio=0
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articulos: []
+    };
+  }
+
+  componentWillMount() {
+    fetch("http://scratchya.com.ar/vue/datos.php")
+      .then(response => {
+        return response.json();
+      })
+      .then(art => {
+        this.setState({ articulos: art });
+      });
+  }
+
   render() {
     return (
       <div>
-        <Reloj />
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Descripción</th>
+              <th>Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.articulos.map(art => {
+              return (
+                <tr key={art.codigo}>
+                  <td>{art.codigo}</td>
+                  <td>{art.descripcion}</td>
+                  <td>{art.precio}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
