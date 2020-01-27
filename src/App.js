dc5a1c4e7a7f735db356a0acf77f55ae78f246d5
Ivan Rogelio Chenoweth Chenoweth <ivan chenoweth@tiempodev.local>
2020-01-26 20:46:15 -0700

    US12: adding the binding to forms

--- src/App.js
+++ src/App.js
@@ -1,67 +1,92 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// http://www.tutorialesprogramacionya.com/reactya/detalleconcepto.php?punto=12&codigo=12&inicio=0
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      articulos: [
-        {
-          codigo: 1,
-          descripcion: "papas",
-          precio: 12.52
-        },
-        {
-          codigo: 2,
-          descripcion: "naranjas",
-          precio: 21
-        },
-        {
-          codigo: 3,
-          descripcion: "peras",
-          precio: 18.2
-        }
-      ]
+      nombre: "",
+      edad: "",
+      estudio: false
     };
-    this.borrar = this.borrar.bind(this);
+    // Enlazamos los métodos que se ejecutarán cuando haya cambios en los controles de formulario
+    // Si no nos lanza: Cannot read property 'setState' of undefined en cada metodo: procesar, cambioXXXX
+    this.procesar = this.procesar.bind(this);
+    this.cambioNombre = this.cambioNombre.bind(this);
+    this.cambioEdad = this.cambioEdad.bind(this);
+    this.cambioEstudio = this.cambioEstudio.bind(this);
   }
+
   render() {
     return (
       <div>
-        <table border="1">
-          <thead>
-            <tr>
-              <th>Código</th>
-              <th>Descripción</th>
-              <th>Precio</th>
-              <th>Borra?</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.articulos.map(elemento => {
-              return (
-                <tr key={elemento.codigo}>
-                  <td>{elemento.codigo}</td>
-                  <td>{elemento.descripcion}</td>
-                  <td>{elemento.precio}</td>
-                  <td>
-                    <button onClick={() => this.borrar(elemento.codigo)}>
-                      Borrar
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <form onSubmit={this.procesar}>
+          <p>
+            Ingrese nombre:
+            <input
+              type="text"
+              value={this.state.nombre}
+              onChange={this.cambioNombre}
+            />
+          </p>
+          <p>
+            Ingrese edad:
+            <input
+              type="number"
+              value={this.state.edad}
+              onChange={this.cambioEdad}
+            />
+          </p>
+          <p>
+            Estudios:
+            <input
+              type="checkbox"
+              value={this.state.estudio}
+              onChange={this.cambioEstudio}
+            />
+          </p>
+          <p>
+            <input type="submit" />
+          </p>
+        </form>
+        <hr />
+        <h3>Datos Ingresados</h3>
+        <p>Nombre:{this.state.nombre}</p>
+        <p>Edad:{this.state.edad}</p>
+        <p>Estudios:{this.state.estudio ? "con estudios" : "sin estudios"}</p>
       </div>
     );
   }
 
-  borrar(cod) {
-    var temp = this.state.articulos.filter(el => el.codigo !== cod);
+  procesar(e) {
+    e.preventDefault();
+    alert(
+      "Dato cargado " +
+        this.state.nombre +
+        " " +
+        +this.state.edad +
+        " " +
+        +this.state.estudio
+    );
+  }
+
+  cambioNombre(e) {
     this.setState({
-      articulos: temp
+      nombre: e.target.value
+    });
+  }
+
+  cambioEdad(e) {
+    this.setState({
+      edad: e.target.value
+    });
+  }
+
+  cambioEstudio(e) {
+    this.setState({
+      estudio: !this.state.estudio
     });
   }
 }
